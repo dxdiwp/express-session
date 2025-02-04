dec8ec989cce2d14cd61348e9d6a79c0f63b54f0
dodi wahyu <dodiwahyu28@gmail.com>
2025-02-04 16:46:46 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import session from "express-session";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // setup session middleware
 app.use(
@@ -11,30 +13,59 @@ app.use(
     saveUninitialized: true,
     cookie: {
       secure: false,
+      maxAge: 60 * 60 * 1000,
     },
   })
 );
 
-// set session data
-app.get("/set-session", (req, res) => {
-  req.session.name = "bapak hitam";
-  req.session.age = 22;
-  res.send("session data has been set");
+// middleware check auth
+const checkAuth = (req, res, next) => {
+  if (!req.session.user) {
+    return res.status(401).json({
+      message: "Anda belum login",
+    });
+  }
+  next();
+};
+
+// login route
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === "admin" && password === "password") {
+    // simpan user ke sesi
+    req.session.user = {
+      username: username,
+      timestamp: new Date(),
+    };
+    res.status(200).json({
+      message: "login sukses",
+      user: req.session.user,
+    });
+  } else {
+    res.status(401).json({
+      message: "username atau password salah",
+    });
+  }
 });
 
-// get session data
-app.get("/get-session", (req, res) => {
-  const { name, age } = req.session;
-  res.send(`user name: ${name}, age: ${age}`);
+// protected route
+app.get("/protected", checkAuth, (req, res) => {
+  res.status(200).json({
+    message: "protected data",
+    user: req.session.user,
+  });
 });
 
 // delete session data
-app.get("/logout", (req, res) => {
+app.post("/logout", (req, res) => {
   req.session.destroy((err) => {
     if (err) {
-      return res.send("gagal menghapus session!");
+      return res.status(500).json({
+        message: "usaha logout gagal",
+      });
     }
-    res.send("session telah dihapus");
+    res.send("logout berhasil");
   });
 });
 
